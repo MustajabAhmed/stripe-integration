00a5bfe362e3db7ef0f518e3ea815c62e320c64d
MustajabAhmed <mustajabahmed02@gmail.com>
2023-07-16 20:04:28 +0500

    Stripe integrated for staic data right now and when user click on the button it will redirect to the checkout page of the stripe

---
+++ src/components/Checkout.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import getStripePromise from "@/lib/stripe"
+
+const products = [
+    {
+        product: 1,
+        name: 'Stripe Product',
+        price: 400,
+        quantity: 3,
+    },
+]
+
+const StripeCheckOutButton = async () => {
+
+    const stripe = await getStripePromise()
+
+    const handleCheckout = async () => {
+
+        const response = await fetch('/api/stripe-session/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            cache: 'no-cache',
+            body: JSON.stringify({
+                products
+            }),
+        })
+
+        const data = await response.json()
+
+        if (data.session) {
+            stripe?.redirectToCheckout({ sessionId: data.session.id })
+        }
+
+    }
+
+    return (
+        <div className="py-5">
+            <button className="bg-green-500 py-3 px-3 rounded-md" onClick={handleCheckout}>
+                Check out
+            </button>
+        </div>
+    )
+}
+
+export default StripeCheckOutButton
